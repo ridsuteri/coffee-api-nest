be0338b2e7bdfb63d6a2f16f5c23aac341633587
Riddhi Suteri <ridsuteri@gmail.com>
2025-02-17 13:19:25 +0530

    playing with routes & queries

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,14 +1,49 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Query,
+  Response,
+} from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get('flavors')
-  findAll() {
-    return ['mocha', 'laate'].toString();
+  findAll(@Response() response, @Query() paginatedQuery) {
+    // return ['mocha', 'laate'].toString();
+    let { limit, offset } = paginatedQuery;
+    return response
+      .status(200)
+      .send(
+        `Will return all coffees in paginated manner, limit: ${limit} offset: ${offset}`,
+      );
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string){
+  findOne(@Param('id') id: string) {
     return `Returning coffee with id: ${id}`;
+  }
+
+  @Post()
+  //   trying status codes here
+  //   @HttpCode(HttpStatus.BAD_REQUEST)
+  create(@Body('name') body) {
+    return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: 'string', @Body() body) {
+    return `This action requires ${id} to be updated with ${JSON.stringify(body)}`;
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: 'string') {
+    return `This action will delete the record with ${id}`;
   }
 }
